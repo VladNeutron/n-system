3e2db420d50f95d9dcd4e3ab02e351d071502a0a
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-04-18 14:08:07 +0300

    bank

--- src/views/BankInfoView.vue
+++ src/views/BankInfoView.vue
@@ -1,10 +1,13 @@
 <template>
+<the-navbar></the-navbar>
+<main class="main-content border-radius-lg ">
   <div class="container-fluid container__padding">
       <div class="bank__title">
           <p class="bank__title__text">Банковская информация</p>
           <p class="bank__title__">Все добавленные Вами счета</p>
       </div>
   </div>
+</main>
 </template>
 
 <script>
