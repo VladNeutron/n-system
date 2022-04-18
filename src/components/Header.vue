b83c8ac976214fa1b4112145dd526190bcfaa55d
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-04-18 19:32:15 +0300

    navbararrows

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -138,6 +138,7 @@
 
 <script>
 import '@/assets/js/soft-ui-dashboard.min.js'
+import PerfectScrollbar from '../assets/js/plugins/perfect-scrollbar.min.js'
 export default {
     methods:{
         // Navbar blur on scroll
@@ -279,6 +280,32 @@ export default {
         if (document.getElementById('navbarBlur')) {
             this.navbarBlurOnScroll('navbarBlur');
         }
+        (function() {
+            var isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+
+            if (isWindows) {
+                // if we are on windows OS we activate the perfectScrollbar function
+                if (document.getElementsByClassName('main-content')[0]) {
+                var mainpanel = document.querySelector('.main-content');
+                var ps = new PerfectScrollbar(mainpanel);
+                };
+
+                if (document.getElementsByClassName('sidenav')[0]) {
+                var sidebar = document.querySelector('.sidenav');
+                var ps1 = new PerfectScrollbar(sidebar);
+                };
+
+                if (document.getElementsByClassName('navbar-collapse')[0]) {
+                var fixedplugin = document.querySelector('.navbar-collapse');
+                var ps2 = new PerfectScrollbar(fixedplugin);
+                };
+
+                if (document.getElementsByClassName('fixed-plugin')[0]) {
+                var fixedplugin = document.querySelector('.fixed-plugin');
+                var ps3 = new PerfectScrollbar(fixedplugin);
+                };
+            };
+            })();
     }
 }
 </script>
