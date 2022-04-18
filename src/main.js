c6a51c3a7443ea5b0f8a1a5c9cdf87313eeb0a26
Valera <milchakov.v@neutron-agency.com>
2022-04-18 12:19:40 +0300

    ix

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import '@/assets/css/nucleo-icons.css'
+import '@/assets/css/nucleo-svg.css'
+import '@/assets/css/soft-ui-dashboard.css'
+import '@/assets/js/core/popper.min.js'
+import '@/assets/js/core/bootstrap.min.js'
+import '@/assets/js/soft-ui-dashboard.min.js'
 createApp(App).use(store).use(router).mount('#app')
