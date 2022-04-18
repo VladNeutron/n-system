42659dcd96772a5337c122d570febd30da7eb8bd
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-04-18 13:17:07 +0300

    nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <the-navbar></the-navbar>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  
   <router-view/>
 </template>
 
