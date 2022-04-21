28f52398ca497ff52caeaa0ceb46b0b364431012
Tony <51391400+rarabog@users.noreply.github.com>
2022-04-21 13:10:33 +0300

    Login reg

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@/assets/css/fonts.css'
-import '@/assets/css/nucleo-icons.css'
-import '@/assets/css/nucleo-svg.css'
-import '@/assets/css/soft-ui-dashboard.css'
-import '@/assets/js/core/popper.min.js'
-import '@/assets/js/core/bootstrap.min.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "@/assets/css/fonts.css";
+import "@/assets/css/nucleo-icons.css";
+import "@/assets/css/nucleo-svg.css";
+import "@/assets/css/soft-ui-dashboard.css";
+import "@/assets/js/core/popper.min.js";
+import "@/assets/js/core/bootstrap.min.js";
 // import '@/assets/js/soft-ui-dashboard.min.js'
-import '@/assets/js/plugins/perfect-scrollbar.min.js'
-import '@/assets/js/plugins/smooth-scrollbar.min.js'
+import "@/assets/js/plugins/perfect-scrollbar.min.js";
+import "@/assets/js/plugins/smooth-scrollbar.min.js";
 
 // Components
-import Navbar from './components/Navbar.vue'
-import Header from './components/Header.vue'
-import DropDownInv from './components/DropDownInv'
+import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
+import DropDownInv from "./components/DropDownInv";
+import Login from "./components/log2.vue";
+import Register from "./components/Login.vue";
 
+const app = createApp(App).use(store).use(router);
 
-const app = createApp(App).use(store).use(router)
+app.component("the-navbar", Navbar);
+app.component("the-header", Header);
+app.component("drop-down", DropDownInv);
+app.component("login", Login);
+app.component("register", Register);
 
-app.component('the-navbar', Navbar)
-app.component('the-header', Header)
-app.component('drop-down', DropDownInv)
-
-app.mount('#app')
-
+app.mount("#app", Login);
