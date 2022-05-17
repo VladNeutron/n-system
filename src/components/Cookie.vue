714b82999ee757aca4fb16ee84b61d01f6bf0826
rarabog <falen546@gmail.com>
2022-05-17 16:58:47 +0300

    fixed cookie

--- src/components/Cookie.vue
+++ src/components/Cookie.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="cookie__body">
-    <div class="card p-3">
+    <div class="card p-4 pb-3">
       <div class="cookie__body-text d-flex gap-3 mb-3">
         <img src="@/assets/css/images/cookie.png" alt="" />
         <p class="m-0 text-start">
           Для повышения удобства использования Neutron System мы используем
-          <a href="#" class="text-info">Cookies</a>. Запретить обработку Cookies
-          можно в настройках Вашего браузера. Пожалуйста, ознакомьтесь с
-          условиями обработки <a href="#" class="text-info">Cookies</a>.
+          <a href="#">Cookies</a>. Запретить обработку Cookies можно в
+          настройках Вашего браузера. Пожалуйста, ознакомьтесь с условиями
+          обработки <a href="#">Cookies</a>.
         </p>
       </div>
       <div class="cookie__body-button">
-        <button class="btn bg-gradient-info m-0">Хорошо, принимаю</button>
+        <button class="btn mb-0">Хорошо, принимаю</button>
       </div>
     </div>
   </div>
@@ -31,7 +31,24 @@ export default {};
   width: 45px;
 }
 .cookie__body-text p {
-  font-size: 15px;
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
   line-height: 22px;
+  letter-spacing: -0.44px;
+}
+a {
+  color: #8baef3;
+}
+button {
+  background: linear-gradient(83.56deg, #7092e0 10.01%, #8baef3 75.36%);
+  box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.12),
+    0px 2px 4px -1px rgba(0, 0, 0, 0.07);
+  border-radius: 8px;
+  color: #fff;
+}
+button:hover {
+  color: #fff;
 }
 </style>
