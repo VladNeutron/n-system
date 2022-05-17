16fc15fccab384e5da24921fc80959a5b9458dc1
rarabog <falen546@gmail.com>
2022-05-17 16:43:20 +0300

    fixed login form

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="modalsss">
     <div class="card">
-      <div
-        class="modal__logo d-flex align-items-center gap-3 p-4 text-start border-bottom"
-      >
+      <div class="modal__logo d-flex align-items-center px-5 py-4 text-start">
         <img src="@/assets/css/icons/logo.svg" alt="" />
         <div>
-          <p class="m-0 fw-bold fs-4">NEUTRON</p>
-          <p class="m-0 fw-">System</p>
+          <p class="m-0 logo-top">NEUTRON</p>
+          <p class="m-0 logo-bottom">System</p>
         </div>
       </div>
 
-      <div class="modal__form text-start w-100">
+      <div class="modal__form text-start w-100 px-5 pb-4">
         <div class="modal__form-name">
-          <h3 class="mt-4">Вход</h3>
-          <p class="fw-light fs-6">Для входа введите данные</p>
+          <h3>Вход</h3>
+          <p>Для входа введите данные</p>
         </div>
         <div class="form-group">
-          <div class="input-group pb-2">
+          <div class="input-group mb-3">
             <input
               type="text"
               class="form-control"
@@ -26,7 +24,7 @@
               aria-describedby="basic-addon1"
             />
           </div>
-          <div class="input-group pb-2">
+          <div class="input-group mb-2">
             <input
               type="password"
               class="form-control"
@@ -38,12 +36,12 @@
               ><img src="@/assets/css/icons/eye.svg" alt=""
             /></span>
           </div>
-          <p class="text-end fw-light fs-6">Забыли пароль?</p>
+          <p class="text-end link-password mb-3">Забыли пароль?</p>
         </div>
         <div class="modal__form-enter">
-          <button class="btn bg-gradient-dark btn-lg w-100">Вход</button>
+          <button class="btn bg-gradient-dark btn-lg w-100 mb-3">Вход</button>
           <p class="text-center m-0">
-            Нет аккаунта? <a class="text-info" href="#">Регистрация</a>
+            Нет аккаунта? <a href="#">Регистрация</a>
           </p>
         </div>
       </div>
@@ -59,7 +57,92 @@ export default {};
 .modalsss {
   width: 460px;
 }
-.card {
-  padding: 32px 50px;
+.modal__logo {
+  border-bottom: 1px solid #e2e8f0;
+  gap: 21px;
+  height: 120px;
+}
+.logo-top {
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 25.9415px;
+  line-height: 120%;
+}
+.logo-bottom {
+  font-family: "Montserrat";
+  font-weight: 400;
+  font-size: 18.16px;
+  line-height: 21.79px;
+}
+.modal__form-name > h3 {
+  margin-top: 32px;
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 33px;
+}
+.modal__form-name > p {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.44px;
+
+  margin-bottom: 24px;
+
+  /* Gray */
+  color: #a0aec0;
+}
+.input-group {
+  height: 48px;
+}
+input {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+
+  /* identical to box height */
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.44px;
+
+  /* Gray */
+  color: #a0aec0;
+}
+.link-password {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  letter-spacing: -0.44px;
+  color: #67748e;
+  cursor: pointer;
+}
+.modal__form-enter button {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 22px;
+}
+.modal__form-enter p,
+.modal__form-enter a {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
+  letter-spacing: -0.44px;
+}
+a {
+  color: #8baef3;
 }
 </style>
