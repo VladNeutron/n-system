fb4a62aa18b648636c0a48e59df41d69a96ba0a3
Uladzislau Leanenka <vladko.l@neutron-agency.com>
2022-05-18 17:29:10 +0300

    btns

---
+++ src/components/inputs/ListSearch.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="form-group m-0">
+        <div class="input-group">
+            <span class="input-group-text"
+                ><img src="@/assets/css/icons/searchIcon.svg" alt=""
+            /></span>
+            <input
+                class="form-control"
+                placeholder="Поиск..."
+                id="search"
+                type="text"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
