06214f0755f7e85972d5dc5301b88d3859100da1
iarchangelneu <milchakov.v@neutron-agency.com>
2022-06-21 12:26:50 +0300

    selectProduct

--- src/components/SelectProductDiscount.vue
+++ src/components/SelectProductDiscount.vue
@@ -1,12 +1,5 @@
 <template>
-  <div
-    class="modal fade"
-    id="SelectDisc"
-    tabindex="-1"
-    role="dialog"
-    aria-labelledby="SelectDisc"
-    aria-hidden="true"
-  >
+  <div class="modal fade" id="SelectDisc" tabindex="-1" role="dialog" aria-labelledby="SelectDisc" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header header-flex">
@@ -16,38 +9,21 @@
               Выделите необходимые товары и нажмите “Готово”
             </p>
           </div>
-          <img
-            src="@/assets/img/closemodal.svg"
-            style="width: 1.25vw; cursor: pointer"
-            alt=""
-            data-bs-dismiss="modal"
-          />
+          <img src="@/assets/img/closemodal.svg" style="width: 1.25vw; cursor: pointer" alt=""
+            data-bs-dismiss="modal" />
         </div>
         <div class="modal-body">
           <div class="search modal__padding">
             <div class="form-group mb-0" style="text-align: left">
-              <label for="search" class="search__label"
-                >Добавление товара по назвванию</label
-              >
-              <input
-                type="text"
-                class="form-control form-control-lg search__input"
-                placeholder="Начните вводить название"
-                id="search"
-                v-model.trim="search"
-              />
+              <label for="search" class="search__label">Добавление товара по назвванию</label>
+              <input type="text" class="form-control form-control-lg search__input"
+                placeholder="Начните вводить название" id="search" v-model.trim="search" />
             </div>
             <div class="search__buttons">
-              <button
-                :class="['left__btn', { active1: onStorage == 'yes' }]"
-                @click="onStorage = 'yes'"
-              >
+              <button :class="['left__btn', { active1: onStorage == 'yes' }]" @click="onStorage = 'yes'">
                 Есть на складе
               </button>
-              <button
-                :class="['right__btn', { active2: onStorage == 'all' }]"
-                @click="onStorage = 'all'"
-              >
+              <button :class="['right__btn', { active2: onStorage == 'all' }]" @click="onStorage = 'all'">
                 Все товары
               </button>
             </div>
@@ -60,26 +36,14 @@
                     <th scope="col" class="th__col" style="border-right: 0">
                       Название товара
                     </th>
-                    <th
-                      scope="col"
-                      class="th__col"
-                      style="border-left: 0; border-right: 0"
-                    >
+                    <th scope="col" class="th__col" style="border-left: 0; border-right: 0">
                       Цвет
                     </th>
-                    <th
-                      scope="col"
-                      class="th__col"
-                      style="border-left: 0; border-right: 0"
-                    >
+                    <th scope="col" class="th__col" style="border-left: 0; border-right: 0">
                       Размер
                     </th>
 
-                    <th
-                      scope="col"
-                      class="th__col"
-                      style="border-left: 0; border-right: 0"
-                    >
+                    <th scope="col" class="th__col" style="border-left: 0; border-right: 0">
                       Штрихкод
                     </th>
                     <th scope="col" class="th__col" style="border-left: 0"></th>
@@ -99,11 +63,9 @@
                       {{ item.barcode }}
                     </td>
                     <td style="border-left: 0">
-                      <button
-                        class="btn add__button"
-                        @click="$event.target.classList.add('added')"
-                      >
-                        Добавить
+                      <button class="btn add__button" @click="provide(item)"
+                        :class="{ added: contains(candidates, item) }">
+
                       </button>
                     </td>
                   </tr>
@@ -113,64 +75,8 @@
           </div>
         </div>
         <div class="modal-footer">
-          <pagination-component :strAmount="10" :filteredArr="filteredProducts" @PaginationReload="reloadPagination"></pagination-component>
-          <!-- <div class="pagination d-flex justify-content-end pe-5">
-            <div class="d-flex align-items-center gap-3 pb-2">
-              <div>
-                <p class="m-0">Показано<span> 1-10 из 324</span></p>
-              </div>
-
-              <div class="page__search-pages d-flex align-content-center">
-                <div class="pagination-container d-flex justify-items-center">
-                  <ul class="pagination pagination-info mb-0 pe-0">
-                    <li class="page-item">
-                      <a
-                        class="page-link"
-                        href="javascript:;"
-                        aria-label="Previous"
-                      >
-                        <span aria-hidden="true"
-                          ><i
-                            class="fa fa-angle-double-left"
-                            aria-hidden="true"
-                          ></i
-                        ></span>
-                      </a>
-                    </li>
-                    <li class="page-item active">
-                      <a class="page-link" href="javascript:;">1</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="javascript:;">2</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="javascript:;">3</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="javascript:;">4</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="javascript:;">5</a>
-                    </li>
-                    <li class="page-item">
-                      <a
-                        class="page-link"
-                        href="javascript:;"
-                        aria-label="Next"
-                      >
-                        <span aria-hidden="true"
-                          ><i
-                            class="fa fa-angle-double-right"
-                            aria-hidden="true"
-                          ></i
-                        ></span>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div> -->
+          <pagination-component :strAmount="10" :filteredArr="filteredProducts" @PaginationReload="reloadPagination">
+          </pagination-component>
         </div>
       </div>
     </div>
@@ -180,9 +86,27 @@
 <script>
 import PaginationComponent from './Pagination/PaginationComponent.vue';
 export default {
+  methods: {
+    reloadPagination(arr) {
+      this.paginationList = arr;
+    },
+    provide(item) {
+      if (this.candidates.indexOf(item) == -1) {
+        this.candidates.push(item);
+      }
+      else {
+        this.candidates.splice(this.candidates.indexOf(item), 1);
+      }
+    },
+    contains(arr, item) {
+      return arr.indexOf(item) != -1;
+
+    }
+  },
   components: { PaginationComponent },
   data() {
     return {
+      candidates: [],
       search: "",
       onStorage: "yes",
       added: false,
@@ -321,11 +245,7 @@ export default {
       });
     },
   },
-  methods: {
-    reloadPagination(arr) {
-      this.paginationList = arr;
-    },
-  }
+
 };
 </script>
 
@@ -333,58 +253,61 @@ export default {
 /* td{
   height: 40px !important;
 } */
-.inv__content{
+.inv__content {
   overflow: auto;
   max-height: 60vh;
 }
+
 .inv__content::-webkit-scrollbar {
-    background: #e2e8f0;
-    border-radius: 0.78vw;
-    width: 0.37vw;
-  }
-  
+  background: #e2e8f0;
+  border-radius: 0.78vw;
+  width: 0.37vw;
+}
+
 .inv__content::-webkit-scrollbar-thumb {
-    border-radius: 0.78vw;
-    background-color: #313860;
-  }
-  
+  border-radius: 0.78vw;
+  background-color: #313860;
+}
+
 .inv__content::-webkit-scrollbar-track {
-    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
-    border-radius: 0.78vw;
-    background-color: #e2e8f0;
-  }
-.inv__block{
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.2);
+  border-radius: 0.78vw;
+  background-color: #e2e8f0;
+}
+
+.inv__block {
   margin-top: 1vw;
 }
+
 .active1 {
-  background: linear-gradient(
-    83.56deg,
-    #7092e0 10.01%,
-    #8baef3 75.36%
-  ) !important;
+  background: linear-gradient(83.56deg,
+      #7092e0 10.01%,
+      #8baef3 75.36%) !important;
   border-radius: 8px 0px 0px 8px !important;
   font-weight: 600 !important;
   font-size: 0.833vw !important;
   color: #ffffff !important;
 }
+
 .active2 {
-  background: linear-gradient(
-    83.56deg,
-    #7092e0 10.01%,
-    #8baef3 75.36%
-  ) !important;
+  background: linear-gradient(83.56deg,
+      #7092e0 10.01%,
+      #8baef3 75.36%) !important;
   border-radius: 0px 8px 8px 0px !important;
   font-weight: 600 !important;
   font-size: 0.833vw !important;
   color: #ffffff !important;
 }
+
 .search__buttons {
   /* margin-top: 0.7vw; */
 }
+
 .form-control {
   border: 1px solid #a0aec0;
   height: 2.344vw;
 }
+
 .left__btn {
   background: transparent;
   border: 1px solid #a0aec0;
@@ -397,6 +320,7 @@ export default {
   color: #2d3748;
   font-weight: 400;
 }
+
 .right__btn {
   background: transparent;
   border: 1px solid #a0aec0;
@@ -408,19 +332,23 @@ export default {
   color: #2d3748;
   font-weight: 400;
 }
+
 .search {
   display: flex;
   justify-content: space-between;
   align-items: end;
 }
+
 .search__label {
   font-weight: 600;
   font-size: 0.729vw;
   color: #2d3748;
 }
+
 .search__input {
   width: 18.031vw;
 }
+
 .added {
   background: transparent !important;
   border-radius: 5px !important;
@@ -431,6 +359,15 @@ export default {
   color: #2d3748 !important;
   text-transform: none !important;
 }
+
+.added::before {
+  content: 'Добавлено' !important;
+}
+
+.add__button::before {
+  content: 'Добавить'
+}
+
 .add__button {
   background: linear-gradient(81.62deg, #313860 2.25%, #151928 79.87%);
   border-radius: 5px;
@@ -442,104 +379,124 @@ export default {
   text-transform: none;
   margin-bottom: 0;
 }
-.pagination.pagination-info .page-item.active > .page-link,
-.pagination.pagination-info .page-item.active > .page-link:focus,
-.pagination.pagination-info .page-item.active > .page-link:hover {
+
+.pagination.pagination-info .page-item.active>.page-link,
+.pagination.pagination-info .page-item.active>.page-link:focus,
+.pagination.pagination-info .page-item.active>.page-link:hover {
   background-image: linear-gradient(83.56deg, #7092e0 10.01%, #8baef3 75.36%);
 }
+
 .header__sec {
   font-weight: 400;
   font-size: 0.833vw;
   color: #a0aec0;
   margin-bottom: 0;
 }
+
 .header__main {
   margin: 0;
   font-weight: 700;
   font-size: 1.563vw;
   color: #252f40;
 }
+
 .header-flex {
   padding: 1.25vw 2.083vw 0.833vw 2.083vw;
   display: flex;
   justify-content: space-between;
   align-items: baseline;
 }
+
 .fade {
-  background: linear-gradient(
-    83.56deg,
-    rgba(112, 146, 224, 0.1) 10.01%,
-    rgba(139, 174, 243, 0.1) 75.36%
-  );
+  background: linear-gradient(83.56deg,
+      rgba(112, 146, 224, 0.1) 10.01%,
+      rgba(139, 174, 243, 0.1) 75.36%);
   /* backdrop-filter: blur(50px); */
 }
+
 .btn-outline-dark {
   padding: 0.625vw 4.219vw 0.625vw 4.219vw;
 }
+
 .bg-gradient-secondary {
   background: linear-gradient(81.62deg, #313860 2.25%, #151928 79.87%);
   border-radius: 8px;
   text-transform: uppercase;
   padding: 0.625vw 4.219vw 0.625vw 4.219vw;
 }
+
 .del__btn {
   margin-right: 3.542vw;
 }
+
 .modal-footer {
   margin-top: -1vw;
   justify-content: right;
   border-top: 0;
 }
+
 .modal__padding {
   padding-top: 0.1vw;
   padding-left: 2.083vw;
   padding-right: 2.083vw;
 }
+
 .modal__title {
   font-weight: 600;
   font-size: 1.042vw;
   color: #2d3748;
 }
+
 .modal-dialog {
   max-width: 48.073vw;
 }
+
 @media screen and (max-width: 1600px) {
-  .inv__content{
+  .inv__content {
     max-height: 52vh;
   }
+
   td,
   th {
     font-size: 12px;
   }
+
   .header__main {
     font-size: 20px;
   }
+
   .header__sec {
     font-size: 14px;
   }
+
   .btn,
   .search__label {
     font-size: 12px;
   }
+
   .right__btn,
   .left__btn {
     font-size: 12px;
   }
+
   .search__input {
     width: 19vw;
     height: 28px;
   }
+
   .active1,
   .active2 {
     font-size: 12px !important;
   }
+
   .form-group {
     margin-bottom: 1.3rem;
   }
 }
+
 @media screen and (max-width: 1400) {
-  .modal-dialog{
+  .modal-dialog {
     max-width: 54.073vw;
-  }  
+  }
 }
 </style>
